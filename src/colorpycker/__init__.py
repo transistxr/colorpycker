dca423d0ab4e15cb305df77c593cb5d3b510e7b2
transistxr <sparklymachine@proton.me>
2024-07-16 17:34:53 +0530

    Added entrypoint

---
+++ src/colorpycker/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+import sys
+from colorpycker import floatingwidget
+
+def main():
+    sys.exit(floatingwidget.run())
--- src/colorpycker/__main__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-import sys
-from colorpycker import floatingwidget
-
-if __name__ == "__main__":
-    sys.exit(floatingwidget.run())
